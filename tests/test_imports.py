ec440a3519f210e7ba616628314e775ec847dede
awais-akhtar <awais918@gmail.com>
2026-08-14 22:02:47 +0100

    Fix test_imports version assertion for 0.1.2

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,7 +5,7 @@ class ImportTests(unittest.TestCase):
     def test_public_api_imports_without_heavy_dependencies(self):
         import visionparse
 
-        self.assertEqual(visionparse.__version__, "0.1.1")
+        self.assertEqual(visionparse.__version__, "0.1.2")
         self.assertTrue(hasattr(visionparse, "extract_prices"))
 
 
